c1175f593d59deba2efadfa3507c10dd1e13dbe1
Ilyas Hallakoglu <ilhallak@gmail.com>
2014-05-13 11:31:54 +0200

    HALLO HELEN

---
+++ HoleInOne/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  HoleInOne
+//
+//  Created by Ilyas Hallak on 13.05.14.
+//  Copyright (c) 2014 Ilyas Hallak. All rights reserved.
+//
+
+#include <iostream>
+// #include "/opt/local/include/GLFW/glfw3.h"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
